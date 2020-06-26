6d3fb042a533c09c92ad138147aed1a297eff478
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-06-25 18:36:40 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int test =6;
+        if (test == 9){
+            System.out.println("yes");
+
+        }
+        else{
+            System.out.println("else");
+        }
+
+    }
+}
